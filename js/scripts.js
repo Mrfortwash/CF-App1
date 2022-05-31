4a9b20b1b5fc581e1c92ce201f06aff37585f821
Mrfortwash <jeremyharris1996@gmail.com>
2022-05-30 21:45:11 -0400

    Array with 3 objects
    Created an array called pokemonList with 3 objects for Pokémon App

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('HELLO WORLD');
 
-let favoriteFood = ('Ribs');
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
+    { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] },
+    { name: 'Charizard', height: 6, type: ['orange', 'water'] }
+];
