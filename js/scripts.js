5300091aaf3fe6feafb5c8af192a7086158e973c
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-03 00:31:23 -0400

    CSS style and JS entry
    JS loop added along with CS styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-
+//list of Pokemon and their height, type(s), and abilities
 let pokemonList = [
     { name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
     { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] },
     { name: 'Charizard', height: 6, type: ['orange', 'water'] }
 ];
+
+
+//displays the list of Pokemon with descriptive text dependent on the Pokemon height.
+for (let i = 0; i < pokemonList.length; i++) {
+
+    if (pokemonList[i].height > 5) {
+
+        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <i> Wow, that's big!</i>" + "<br>" + "<br>");
+    }
+    else if (pokemonList[i].height < 5) {
+
+        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <span>This pokemon is small!</span>" + "<br>" + "<br>");
+    }
+}
