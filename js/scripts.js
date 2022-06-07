c84cb69b65589f88cf178cd373d0622c1d218c0d
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-06 22:13:33 -0400

    for each loop and IIFE
    turned pokemonList array into a "forEach" loop from "for"
    
    wrapped the function with an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-//list of Pokemon and their height, type(s), and abilities
+// list of Pokemon and their height, type(s), and abilities
 let pokemonList = [
     { name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
     { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] },
@@ -7,14 +7,24 @@ let pokemonList = [
 
 
 //displays the list of Pokemon with descriptive text dependent on the Pokemon height.
-for (let i = 0; i < pokemonList.length; i++) {
+pokemonList.forEach((function (pokemon) {
+    document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + '</p>');
+}))();
+// for (let i = 0; i < pokemonList.length; i++) {
 
-    if (pokemonList[i].height > 5) {
+//     if (pokemonList[i].height > 5) {
 
-        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <i> Wow, that's big!</i>" + "<br>" + "<br>");
-    }
-    else if (pokemonList[i].height < 5) {
+//         document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <i> Wow, that's big!</i>" + "<br>" + "<br>");
+//     }
+//     else if (pokemonList[i].height < 5) {
 
-        document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <span>This pokemon is small!</span>" + "<br>" + "<br>");
-    }
-}
+//         document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ")" + " <span>This pokemon is small!</span>" + "<br>" + "<br>");
+//     }
+
+// let myArray = ['1', '2', '3', '4', '5', '6'];
+// for (let i = 0; i < myarray.length; i++);
+// console.log(myarray)
+
+// myArray.forEach(function (item) {
+//     console.log(item)
+// }); 
