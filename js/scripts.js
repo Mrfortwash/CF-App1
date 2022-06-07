7430b1c01e3831281c2fbe2f7f5a57811f0439db
Mrfortwash <jeremyharris1996@gmail.com>
2022-06-07 14:43:50 -0400

    put conditional back in loop
    previously removed conditional-- adding back into loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,12 @@
 // list of Pokemon and their height, type(s), and abilities
-let pokemonList = [
-    { name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
-    { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] },
-    { name: 'Charizard', height: 6, type: ['orange', 'water'] }
-];
-
+// let pokemonList = [
+//     { name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
+//     { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] },
+//     { name: 'Charizard', height: 6, type: ['orange', 'water'] }
+// ];
 
 //displays the list of Pokemon with descriptive text dependent on the Pokemon height.
-pokemonList.forEach((function (pokemon) {
-    document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + '</p>');
-}))();
+
 // for (let i = 0; i < pokemonList.length; i++) {
 
 //     if (pokemonList[i].height > 5) {
@@ -28,3 +25,37 @@ pokemonList.forEach((function (pokemon) {
 // myArray.forEach(function (item) {
 //     console.log(item)
 // }); 
+let pokemonRepository = (function () {
+    let pokemonList = [{ name: 'Pikachu', height: 2, type: ['blue', 'normal'] },
+    { name: 'Bulbasaur', height: 4, type: ['red', 'fire'] }, '</p>',
+    { name: 'Charizard', height: 6, type: ['orange', 'water'] }]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
+
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.add({ name: 'Kricketune', height: 1, types: ['bug', 'flying'], });
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    if (pokemon.height > 5) {
+        document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + ')-Wow!-that\'s big</p>');
+    } else {
+        document.write('<p>' + pokemon.name, '(height: ' + pokemon.height + ')</p>');
+    }
+});
+
